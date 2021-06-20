f240d4ad3e12b46bbd82f0d243465dbf949fb1d9
hongyu101094 <hongyushi101094aaaa@gmail.com>
2021-06-20 19:46:03 +0800

    0620

--- js/scrollhideheader.js
+++ js/scrollhideheader.js
@@ -2,9 +2,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("navbar")[0].style.top = "0";
+    document.getElementsByClassName("topheader")[0].style.top = "0";
   } else {
-    document.getElementsByClassName("navbar")[0].style.top = "-300px";
+    document.getElementsByClassName("topheader")[0].style.top = "-300px";
   }
   prevScrollpos = currentScrollPos;
 };
